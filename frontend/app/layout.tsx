620226180682afcf7e8c8b8b8c8737176cfd7924
Tarun-Khilani <khilanitarun@gmail.com>
2024-12-08 18:10:43 +0530

    Fix layout viewport issue

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,18 +1,19 @@
 import "../styles/global.css";
 import "../styles/custom.css";
-import { Metadata } from "next";
-import { Inter, Roboto, Montserrat, Open_Sans, Poppins } from 'next/font/google';
+import { Metadata, Viewport } from "next";
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
-const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] });
-const montserrat = Montserrat({ subsets: ['latin'] });
-const openSans = Open_Sans({ subsets: ['latin'] });
-const poppins = Poppins({ weight: ['400', '700'], subsets: ['latin'] });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
 
 export const metadata: Metadata = {
   title: "Chanim",
   description: "Chanim - AI powered infographic generator",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({
